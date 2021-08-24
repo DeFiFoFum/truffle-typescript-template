77c66311d1fe1782af824f7063ee3de7c3651027
defifofum <defifofum0@gmail.com>
2021-08-24 14:32:31 -0400

    fix: Migration script

--- migrations/2_deploy_mock_token.js
+++ migrations/2_deploy_mock_token.js
@@ -1,5 +1,7 @@
+
 const MockToken = artifacts.require("MockToken");
+const { ether } = require('@openzeppelin/test-helpers');
 
 module.exports = function (deployer) {
-  deployer.deploy(MockToken);
+  deployer.deploy(MockToken, ether('1000'));
 };
